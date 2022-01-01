ce8a940e1ef7ac1e7c6a6a5a4ed10852c7de2b4c
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2022-01-01 20:46:27 +0900

    マルチログイン作成

--- routes/admin.php
+++ routes/admin.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\Owner\Admin\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Owner\Admin\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Owner\Admin\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Owner\Admin\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Owner\Admin\Auth\NewPasswordController;
-use App\Http\Controllers\Owner\Admin\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Owner\Admin\Auth\RegisteredUserController;
-use App\Http\Controllers\Owner\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Admin\Auth\NewPasswordController;
+use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Admin\Auth\RegisteredUserController;
+use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,12 +22,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return view('admin.dashboard');
+})->middleware(['auth:admin'])->name('dashboard');
 
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
@@ -61,24 +61,24 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.update');
 
 Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
-                ->middleware('auth')
+                ->middleware('auth:admin')
                 ->name('verification.notice');
 
 Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->middleware(['auth:admin', 'signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
+                ->middleware(['auth:admin', 'throttle:6,1'])
                 ->name('verification.send');
 
 Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->middleware('auth')
+                ->middleware('auth:admin')
                 ->name('password.confirm');
 
 Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
-                ->middleware('auth');
+                ->middleware('auth:admin');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
+                ->middleware('auth:admin')
                 ->name('logout');
